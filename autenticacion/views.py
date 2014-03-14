fb3ebb9d929bc1b3df051cb6f46ef22bf1212ff1
Luis Crespo <luiscrisf@gmail.com>
2014-03-13 20:52:28 -0430

    Agrega diseno de interfaz de calendario de plan quirurgico

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def inicio(request):
+	''' Controlador correspondiente a la pagina de inicio de la aplicacion 
+
+	Parametros:
+	request -> Solicitud HTTP '''
+
 	return render_to_response('autenticacion/inicio.html')
--- quirofanos_cmsb/settings.py
+++ quirofanos_cmsb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'autenticacion',
+    'plan_quirurgico',
 )
 
 MIDDLEWARE_CLASSES = (
