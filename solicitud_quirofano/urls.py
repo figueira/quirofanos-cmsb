33b9fe0041acb460ed429d515ee352049877696a
Luis Crespo <luiscrisf@gmail.com>
2014-03-15 12:20:09 -0430

    Agrega diseno de interfaz de mis solicitudes

--- solicitud_quirofano/urls.py
+++ solicitud_quirofano/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('solicitud_quirofano.views',
-    url(r'^solicitud/$', 'solicitud', name='solicitud'),
+    url(r'^solicitud$', 'solicitud', name='solicitud'),
+    url(r'^mis_solicitudes$', 'mis_solicitudes', name='mis_solicitudes'),
 )
--- solicitud_quirofano/views.py
+++ solicitud_quirofano/views.py
@@ -4,9 +4,17 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def solicitud(request):
-	 ''' Controlador correspondiente a la pagina de solicitud de quirofanos
+	 ''' Controlador correspondiente a la pagina del formulario de una nueva solicitud de quirofano
 
 	 Parametros:
 	 request -> Solicitud HTTP '''
 
 	 return render_to_response('solicitud_quirofano/solicitud.html')
+
+def mis_solicitudes(request):
+	 ''' Controlador correspondiente a la pagina del listado de solicitudes realizadas por el medico
+
+	 Parametros:
+	 request -> Solicitud HTTP '''
+
+	 return render_to_response('solicitud_quirofano/mis_solicitudes.html')
