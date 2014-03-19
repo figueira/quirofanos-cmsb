683793320f86645362a82b3d69df893b2f4807df
Luis Crespo <luiscrisf@gmail.com>
2014-03-18 21:26:54 -0430

    Agrega diseno de cambio de status de operacion, agrega dragscroll al diseno de plan quirurgico para solicitantes

--- plan_quirurgico/static/plan_quirurgico/js/plan_dia_obs.js
+++ plan_quirurgico/static/plan_quirurgico/js/plan_dia_obs.js
@@ -2,4 +2,23 @@ $(document).ready(function() {
 	// Seleccionar seccion en menu de navegacion
 	$(".navegacion").removeClass("active");
 	$("#seccion-plan-quirurgico").addClass("active");
+
+	// Cambio de estado de operacion
+	$(".btn-estado-operacion").click(function() {
+		if ($(this).attr("id") != "btn-en-habitacion") {
+			if (!$(".select-en-habitacion").hasClass("hidden")) {
+				$(".select-en-habitacion").addClass("hidden");
+			}
+		}else {
+			$(".select-en-habitacion").removeClass("hidden");
+		}
+		$(".btn-estado-operacion").removeClass("btn").removeClass("btn-danger").removeClass("btn-warning").removeClass("btn-success").removeClass("btn-default");
+		var botones = $(this).parent().children();
+		$(botones[0]).addClass("btn").addClass("btn-danger").removeAttr("disabled");
+		$(botones[1]).addClass("btn").addClass("btn-warning").removeAttr("disabled");
+		$(botones[2]).addClass("btn").addClass("btn-success").removeAttr("disabled");
+		$(botones[3]).addClass("btn").addClass("btn-success").removeAttr("disabled");
+		$(this).removeClass("btn").removeClass("btn-danger").removeClass("btn-warning").removeClass("btn-success").removeClass("btn-default");
+		$(this).addClass("btn").addClass("btn-default").attr("disabled", "disabled");
+	});
 });
