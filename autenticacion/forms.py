9f8b0116eea0476aeb54bd0bfea707df3561a454
Azocar <daniel.azocar18@gmail.com>
2014-03-27 18:36:29 -0430

    Agrega registro_medico, que ya registra al medico con excepcion de sus especialidades

--- autenticacion/forms.py
+++ autenticacion/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from quirofanos_cmsb.models import Especializacion
+
+# Generos
+GENERO = (
+	('F', u'Femenino'),
+	('M', u'Masculino'),
+	)
+
 class InicioSesionForm(forms.Form):
     nombre_usuario = forms.CharField(max_length=30)
     contrasena = forms.CharField(widget=forms.PasswordInput)
@@ -11,4 +19,18 @@ class RegistroDepartamentoForm(forms.Form):
 	email_departamento = forms.EmailField(max_length=75)
 	nombre_usuario_departamento = forms.CharField(max_length=30)
 	contrasena_departamento = forms.CharField(widget=forms.PasswordInput)
+	contrasena_confirmacion = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistroMedicoForm(forms.Form):
+	nombre_medico = forms.CharField(max_length=20)
+	apellido_medico = forms.CharField(max_length=20)
+	cedula_medico = forms.CharField(max_length=12)
+	especialidad_medico = forms.ModelMultipleChoiceField(queryset=Especializacion.objects.all())
+	genero_medico = forms.ChoiceField(widget=forms.RadioSelect, choices=GENERO)
+	codigo_telefono = forms.CharField(max_length=4, min_length=4)    
+	telefono_medico = forms.CharField(max_length=7, min_length=7)
+	email_medico = forms.EmailField(max_length=75)
+	nombre_usuario_medico = forms.CharField(max_length=30)
+	contrasena_medico = forms.CharField(widget=forms.PasswordInput)
 	contrasena_confirmacion = forms.CharField(widget=forms.PasswordInput)
