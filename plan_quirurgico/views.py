ddcd3ee7d10a349cdb91d60456ea2aef3e211eef
Luis Crespo <luiscrisf@gmail.com>
2014-03-29 21:44:48 -0430

    pull

--- plan_quirurgico/views.py
+++ plan_quirurgico/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+
+from quirofanos_cmsb.models import Cuenta
 
 def calendario(request):
 	''' Controlador correspondiente al calendario de disponibilidad de quirofanos por mes
 
 	Parametros:
 	request -> Solicitud HTTP '''
+	nombre_usuario = request.user.username
+	privilegio = Cuenta.objects.get(usuario=).privilegio
 
 	return render_to_response('plan_quirurgico/calendario.html')
 
