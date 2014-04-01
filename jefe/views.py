7ea33673ea4310c671ce7a38f2e3ca495f74447d
María José Ramos <mjramos91@gmail.com>
2014-04-01 19:08:03 -0430

    Inicio de Solicitudes de Registro

--- jefe/views.py
+++ jefe/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 
+from django.models import Cuenta
 # Create your views here.
 
+@require_http_methods(["GET","POST"])
 def solicitudes_usuarios(request):
 	''' Controlador correspondiente al listado de solicitudes de usuarios
 
 	Parametros:
 	request -> Solicitud HTTP '''
+    datos = {}
+    if request.method == 'GET':
+        lista_solicitudes_usuario_pendientes= Cuenta.objects.all().filter(estado='P')
+        lista_solicitudes_usuario_aprobadas = Cuenta.objects.all().filter(estado='A')
+        lista_solicitudes_usuario_rechazadas = Cuenta.objects.all().filter(estado='R')
 
-	return render_to_response('jefe/solicitudes_usuarios.html')
+    datos['lista_solicitudes_usuario_pendientes'] = lista_solicitudes_usuario_pendientes
+    datos['lista_solicitudes_usuario_aprobadas'] = lista_solicitudes_usuario_aprobadas
+    datos['lista_solicitudes_usuario_rechazadas'] = lista_solicitudes_usuario_rechazadas
+
+	return render_to_response('jefe/solicitudes_usuarios.html', datos, context_instance=RequestContext(request))
