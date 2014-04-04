8832043f92b962705773f131f08b201603c4991e
Luis Crespo <luiscrisf@gmail.com>
2014-04-04 15:47:56 -0430

    Agrega cambios irrelevantes

--- jefe/views.py
+++ jefe/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
 
-from django.models import Cuenta
-# Create your views here.
+from quirofanos_cmsb.models import Cuenta
 
 @require_http_methods(["GET","POST"])
 def solicitudes_usuarios(request):
-	''' Controlador correspondiente al listado de solicitudes de usuarios
+    ''' Controlador correspondiente al listado de solicitudes de usuarios
 
-	Parametros:
-	request -> Solicitud HTTP '''
+    Parametros:
+    request -> Solicitud HTTP '''
     datos = {}
     if request.method == 'GET':
         lista_solicitudes_usuario_pendientes= Cuenta.objects.all().filter(estado='P')
@@ -20,4 +20,4 @@ def solicitudes_usuarios(request):
         datos['lista_solicitudes_usuario_aprobadas'] = lista_solicitudes_usuario_aprobadas
         datos['lista_solicitudes_usuario_rechazadas'] = lista_solicitudes_usuario_rechazadas
 
-	return render_to_response('jefe/solicitudes_usuarios.html', datos, context_instance=RequestContext(request))
+        return render_to_response('jefe/solicitudes_usuarios.html', datos, context_instance=RequestContext(request))
